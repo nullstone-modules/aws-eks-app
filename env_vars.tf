1e7c5516a0ec3b608c2f375f6e67f6e412b5631b
Brad Sickles <brad.sickles@gmail.com>
2026-02-17 10:39:19 -0500

    generate module scaffold

--- env_vars.tf
+++ env_vars.tf
@@ -19,6 +19,13 @@ EOF
 }
 
 locals {
+  cap_env_vars = {
+    for item in local.capabilities.env : "${local.cap_env_prefixes[item.cap_tf_id]}${item.name}" => item.value
+  }
+  cap_secrets = {
+    for item in local.capabilities.secrets : "${local.cap_env_prefixes[item.cap_tf_id]}${item.name}" => sensitive(item.value)
+  }
+
   standard_env_vars = tomap({
     NULLSTONE_STACK         = data.ns_workspace.this.stack_name
     NULLSTONE_APP           = data.ns_workspace.this.block_name
@@ -29,8 +36,9 @@ locals {
     NULLSTONE_PRIVATE_HOSTS = join(",", local.private_hosts)
   })
 
-  input_env_vars = merge(local.standard_env_vars, local.cap_env_vars, var.env_vars)
-  input_secrets  = merge(local.cap_secrets, var.secrets)
+  input_env_vars    = merge(local.standard_env_vars, local.cap_env_vars, var.env_vars)
+  input_secrets     = merge(local.cap_secrets, var.secrets)
+  input_secret_keys = nonsensitive(concat(keys(local.cap_secrets), keys(var.secrets)))
 }
 
 data "ns_env_variables" "this" {
@@ -38,8 +46,19 @@ data "ns_env_variables" "this" {
   input_secrets       = local.input_secrets
 }
 
+// ns_secret_keys.this is used to calculate a set of secrets to add to aws secrets manager
+// The resulting "secret_keys" attribute must be known at plan time
+// This doesn't need to do a full interpolation because we only care about which inputs need to be added to aws secrets manager
+// ns_secret_keys.input_env_variables should contain only var.env_vars since they could contain interpolation that promotes them to sensitive
+// We exclude "local.cap_env_vars" because capabilities must use "cap_secrets" to create secrets
+data "ns_secret_keys" "this" {
+  input_env_variables = var.env_vars
+  input_secret_keys   = local.input_secret_keys
+}
+
 locals {
-  secret_keys  = data.ns_env_variables.this.secret_keys
-  all_secrets  = data.ns_env_variables.this.secrets
-  all_env_vars = data.ns_env_variables.this.env_variables
+  secret_keys          = data.ns_secret_keys.this.secret_keys
+  all_secrets          = data.ns_env_variables.this.secrets
+  all_env_vars         = data.ns_env_variables.this.env_variables
+  existing_secret_refs = [for key, ref in data.ns_env_variables.this.secret_refs : { name = key, valueFrom = ref }]
 }
